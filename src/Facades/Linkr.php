2117ff045531dd279a776ba33be80ab1e82cd8ff
Jérémy Gaulin <gabsource@users.noreply.github.com>
2020-03-30 15:26:15 +0400

    Add comments for PHPDOc

--- src/Facades/Linkr.php
+++ src/Facades/Linkr.php
@@ -2,8 +2,16 @@
 
 namespace Bnb\Laravel\Linkr\Facades;
 
+use Bnb\Linkr\ShortUrl;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static ShortUrl short(string $message)
+ * @method static ShortUrl info(string $alias)
+ * @method static void delete(string $alias)
+ *
+ * @see \Bnb\Linkr\Client
+ */
 class Linkr extends Facade
 {
 
